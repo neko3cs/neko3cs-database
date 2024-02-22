123e5b5faabe86bdd6fbd7354d64ae9cb8950ca2
neko3cs <minaton.0722.net@gmail.com>
2024-02-22 16:13:37 +0900

    feature: Leteral path to $PWD

--- Set-SQLServerDatabaseOnDocker.ps1
+++ Set-SQLServerDatabaseOnDocker.ps1
@@ -1,11 +1,10 @@
-#!pwsh
+#!/usr/local/bin pwsh
 
-docker pull mcr.microsoft.com/mssql/server:latest
-
+docker pull mcr.microsoft.com/mssql/server:latest &&
 docker run `
     -e 'ACCEPT_EULA=Y' `
     -e 'SA_PASSWORD=P@ssword!' `
     -p 1433:1433 `
     --name sql1 `
-    -v /c/src/neko3cs-database:/tmp `
+    -v ${PWD}:/tmp `
     -d mcr.microsoft.com/mssql/server:latest
